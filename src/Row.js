2e18921f52ab1b17e054c8dc7623d56bf818eec9
Bivek Gurung <bivek.tamu@gmail.com>
2020-09-23 11:41:31 +1000

    Trailer component added to play trailers of movies

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import './Row.css'
 import axios from './axios';
-
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer'
 
 
 function Row({ title, fetchUrl, isLargeRow }) {
@@ -9,6 +10,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
     const base_url = 'https://image.tmdb.org/t/p/original/';
 
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
 
     useEffect(() => {
 
@@ -21,6 +23,31 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+            autoplay: 1,
+        }
+
+    };
+
+    const handleOnClick = (movie) => {
+        console.log(movie);
+        console.log(trailerUrl);
+        if (trailerUrl) {
+            setTrailerUrl('');
+        }
+        else {
+            movieTrailer(movie?.name || movie?.title || "")
+                .then(url => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+
+                }).catch(error => console.log(error));
+        }
+    }
+
 
     return (
         <div className='row'>
@@ -29,11 +56,19 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 {movies?.map(movie => (
                     <img
                         key={movie.id}
+                        onClick={() => handleOnClick(movie)}
                         className={`row__poster${isLargeRow ? " row__posterLarge" : ""}`}
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                         alt={movie.title ? movie.title : movie.name} />
                 ))}
             </div>
+
+            {trailerUrl && <>
+                <div className="trailer">
+                    <a className="close__btn" onClick={() => setTrailerUrl('')}></a>
+                    <Youtube videoId={trailerUrl} opts={opts} />
+                </div>
+            </>}
 
         </div>
     )
